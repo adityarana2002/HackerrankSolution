d24cafc481f20d38f5e0b4c3b9b7ad879906ab34
Aditya rana <96383279+adityarana2002@users.noreply.github.com>
2023-09-18 00:01:34 +0530

    Create java string token.java

---
+++ java string token.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+        s = s.trim();
+          if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        
+         String [] words = s.split("[^a-zA-Z]+");
+        // split is used to break the string sentence in word and insert in array form ...
+        System.out.println(words.length);
+        for(String word:words){
+            
+            System.out.println(word);
+        }
+    }
+}
+
